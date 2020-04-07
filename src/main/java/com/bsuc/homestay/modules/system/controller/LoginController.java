f93c32719138a6b9d5a1f527aacfcfa83e01e8c7
yyhuang <yyhuang@amarsoft.com>
2020-04-08 04:26:34 +0800

    feat:(登录)：登录成功

--- src/main/java/com/bsuc/homestay/modules/system/controller/LoginController.java
+++ src/main/java/com/bsuc/homestay/modules/system/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.bsuc.homestay.modules.system.controller;
 import com.bsuc.homestay.base.Result;
 import com.bsuc.homestay.entity.Admin;
 import com.bsuc.homestay.service.AdminService;
-import com.google.common.collect.Maps;
 import com.mysql.cj.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 /**
  *
@@ -33,17 +30,18 @@ public class LoginController {
     AdminService adminService;
     //登录 通过账号密码
     @RequestMapping(value = "/login",produces = {"application/json;charset=UTF-8"})
-    public Result loginMain(Admin admin ){
-        Result result = new Result();
+    public Result loginMain(Admin admin ) throws Exception{
+
+
+        Result<Admin> result = new Result<Admin>();
         result.setSuccess(false);
         result.setDetail(null);
-        Admin admin1 = new Admin();
         if(StringUtils.isNullOrEmpty(admin.getAdminUsername()) || StringUtils.isNullOrEmpty(admin.getAdminPassword())){
-            return null;
+            result.setMsg("用户名或密码不能为空");
         }
-        Map<String,Object> map = Maps.newHashMap();
         try {
-            admin1 = adminService.login(admin);
+            //TODO 需要返回的空对象进行处理
+            Admin admin1 = adminService.login(admin);
             if(admin1 == null){
                 result.setMsg("用户名或密码错误");
             }else{
--- src/main/java/com/bsuc/homestay/service/impl/AdminServiceImpl.java
+++ src/main/java/com/bsuc/homestay/service/impl/AdminServiceImpl.java
@@ -27,16 +27,20 @@ public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements
 
 
     @Override
-    public Admin login(Admin admin) {
+    public Admin login(Admin admin){
+
+        //TODO 注意这边有空指针异常，需要处理
         HashMap<String, Object> map = new HashMap<>();
         map.put("admin_Username", admin.getAdminUsername());
         map.put("admin_Password", admin.getAdminPassword());
         List<Admin> admin2 = adminMapper.selectByMap(map);
+        System.out.println(admin2.get(0));
         if(admin2.size()>0){
             admin2.forEach(System.out::println);
             return admin2.get(0);
         }
-        return admin2.get(0);
+        return null;
+
     }
 
     @Override
